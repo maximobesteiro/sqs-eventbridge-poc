b12e9f7eea48002f92021775b37454db4825341c
Máximo Besteiro <machibest@gmail.com>
2024-04-04 14:33:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# Welcome to your CDK TypeScript project
+# SQS Evenbridge POC
 
-This is a blank project for CDK development with TypeScript.
+This project is an example of how to use SQS and Evenbridge with AWS CDK.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+The `cdk.json` file tells the CDK Toolkit how to execute this app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `npx cdk deploy` deploy this stack to your default AWS account/region
+- `npx cdk diff` compare deployed stack with current state
+- `npx cdk synth` emits the synthesized CloudFormation template
+- `npx cdk destroy` destroy the stack (all resources will be deleted and data will be lost, but we make sure we don't incur additional charges)
--- lib/sqs-evenbridge-poc-stack.ts
+++ lib/sqs-evenbridge-poc-stack.ts
@@ -1,16 +1,81 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as events from "aws-cdk-lib/aws-events";
+import * as pipes from "aws-cdk-lib/aws-pipes";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as logs from "aws-cdk-lib/aws-logs";
 
 export class SqsEvenbridgePocStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // SQS Queue
+    const queue = new sqs.Queue(this, "EventQueue", {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SqsEvenbridgePocQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Grant EventBridge permission to read from the queue
+    const eventBridgePrincipal = new iam.ServicePrincipal(
+      "events.amazonaws.com"
+    );
+    queue.grantConsumeMessages(eventBridgePrincipal);
+
+    // Create an EventBridge Pipe
+    const pipe = new pipes.Pipe(this, "MyPipe");
+    pipe.addEventSource(queue); // Attach the SQS queue as the source
+
+    // Lambda Functions for type A and B
+    const lambdaA = new lambda.Function(this, "LambdaA", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "typeA.handler", // file is "typeA", function is "handler"
+      logRetention: logs.RetentionDays.ONE_WEEK, // Customize log retention
+    });
+
+    const lambdaB = new lambda.Function(this, "LambdaB", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "typeB.handler", // file is "typeB", function is "handler"
+      logRetention: logs.RetentionDays.ONE_WEEK,
+    });
+
+    // EventBridge Rule for type A events
+    const ruleA = new events.Rule(this, "RuleA", {
+      description: "Route events with type A to Lambda A",
+      eventPattern: {
+        source: [queue.queueName],
+        detail: {
+          type: ["A"],
+        },
+      },
+    });
+    ruleA.addTarget(new targets.LambdaFunction(lambdaA));
+
+    // EventBridge Rule for type B events
+    const ruleB = new events.Rule(this, "RuleB", {
+      description: "Route events with type B to Lambda B",
+      eventPattern: {
+        source: [queue.queueName],
+        detail: {
+          type: ["B"],
+        },
+      },
+    });
+    ruleB.addTarget(new targets.LambdaFunction(lambdaB));
+
+    // EventBridge Rule for all events (logging)
+    const loggingRule = new events.Rule(this, "LoggingRule", {
+      description: "Log all events to CloudWatch",
+      eventPattern: {
+        source: [queue.queueName],
+      },
+    });
+
+    //  CloudWatch Log Group
+    const logGroup = new logs.LogGroup(this, "EventBridgeLogs");
+    loggingRule.addTarget(new targets.CloudWatchLogGroup(logGroup));
   }
 }
