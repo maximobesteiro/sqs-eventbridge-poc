456f124387a374895c8cd332902455591dd5b9aa
Máximo Besteiro <machibest@gmail.com>
2024-04-06 15:52:43 +0200

    Fixed stack implementation to correctly route events to desired targets

--- lib/sqs-evenbridge-poc-stack.ts
+++ lib/sqs-evenbridge-poc-stack.ts
@@ -13,42 +13,51 @@ export class SqsEvenbridgePocStack extends cdk.Stack {
     super(scope, id, props);
 
     // SQS Queue
-    const queue = new sqs.Queue(this, "EventQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
-    });
+    const queue = new sqs.Queue(this, "event-queue");
 
     // Event Bus
-    const eventBus = new events.EventBus(this, "EventBus");
+    const eventBus = new events.EventBus(this, "event-bus", {
+      eventBusName: "sqs-event-bridge-bus",
+    });
 
     // Source policy
-    const sourcePolicy = new iam.PolicyStatement({
-      actions: [
-        "sqs:ReceiveMessage",
-        "sqs:DeleteMessage",
-        "sqs:GetQueueAttributes",
+    const sourcePolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          resources: [queue.queueArn],
+          actions: [
+            "sqs:ReceiveMessage",
+            "sqs:DeleteMessage",
+            "sqs:GetQueueAttributes",
+          ],
+          effect: iam.Effect.ALLOW,
+        }),
       ],
-      resources: [queue.queueArn],
-      effect: iam.Effect.ALLOW,
     });
 
     // Target policy
-    const targetPolicy = new iam.PolicyStatement({
-      actions: ["events:PutEvents"],
-      resources: [eventBus.eventBusArn],
-      effect: iam.Effect.ALLOW,
+    const targetPolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          resources: [eventBus.eventBusArn],
+          actions: ["events:PutEvents"],
+          effect: iam.Effect.ALLOW,
+        }),
+      ],
     });
 
     // Create a Pipe role
-    const pipeRole = new iam.Role(this, "PipeRole", {
+    const pipeRole = new iam.Role(this, "pipe-role", {
       assumedBy: new iam.ServicePrincipal("pipes.amazonaws.com"),
+      inlinePolicies: {
+        sourcePolicy,
+        targetPolicy,
+      },
     });
 
-    // Attach the policy to the role
-    pipeRole.addToPolicy(sourcePolicy);
-    pipeRole.addToPolicy(targetPolicy);
-
     // Create an EventBridge Pipe
-    const pipe = new pipes.CfnPipe(this, "SQSEventBridgePipe", {
+    const pipe = new pipes.CfnPipe(this, "SQS-event-bridge-pipe", {
+      name: "sqs-event-bridge-pipe",
       roleArn: pipeRole.roleArn,
       source: queue.queueArn,
       sourceParameters: {
@@ -58,30 +67,38 @@ export class SqsEvenbridgePocStack extends cdk.Stack {
         },
       },
       target: eventBus.eventBusArn,
+      targetParameters: {
+        inputTemplate: `{
+          "messageId": "<$.messageId>",
+          "type": "<$.body.type>",
+          "data": <$.body.data>,
+          "originalEvent": <aws.pipes.event.json>
+        }`,
+      },
     });
 
     // Lambda Functions for type A
-    const lambdaA = new lambda.Function(this, "LambdaA", {
-      runtime: lambda.Runtime.NODEJS_16_X,
+    const lambdaA = new lambda.Function(this, "lambda-A", {
+      runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
       handler: "typeA.handler", // file is "typeA", function is "handler"
       logRetention: logs.RetentionDays.ONE_WEEK, // Customize log retention
     });
 
     // Lambda Functions for type B
-    const lambdaB = new lambda.Function(this, "LambdaB", {
-      runtime: lambda.Runtime.NODEJS_16_X,
+    const lambdaB = new lambda.Function(this, "lambda-B", {
+      runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
       handler: "typeB.handler", // file is "typeB", function is "handler"
       logRetention: logs.RetentionDays.ONE_WEEK,
     });
 
     // EventBridge Rule for type A events
-    const ruleA = new events.Rule(this, "RuleA", {
+    const ruleA = new events.Rule(this, "rule-A", {
       description: "Route events with type A to Lambda A",
       eventBus: eventBus,
       eventPattern: {
-        source: [queue.queueName],
+        source: ["Pipe sqs-event-bridge-pipe"],
         detail: {
           type: ["A"],
         },
@@ -90,10 +107,11 @@ export class SqsEvenbridgePocStack extends cdk.Stack {
     });
 
     // EventBridge Rule for type B events
-    const ruleB = new events.Rule(this, "RuleB", {
+    const ruleB = new events.Rule(this, "rule-B", {
       description: "Route events with type B to Lambda B",
       eventBus: eventBus,
       eventPattern: {
+        source: ["Pipe sqs-event-bridge-pipe"],
         detail: {
           type: ["B"],
         },
@@ -102,14 +120,14 @@ export class SqsEvenbridgePocStack extends cdk.Stack {
     });
 
     //  CloudWatch Log Group
-    const logGroup = new logs.LogGroup(this, "EventBridgeLogs");
+    const logGroup = new logs.LogGroup(this, "event-bridge-logs");
 
     // EventBridge Rule for all events (logging)
-    const loggingRule = new events.Rule(this, "LoggingRule", {
+    const loggingRule = new events.Rule(this, "logging-rule", {
       description: "Log all events to CloudWatch",
       eventBus: eventBus,
       eventPattern: {
-        source: [queue.queueName],
+        source: ["Pipe sqs-event-bridge-pipe"],
       },
       targets: [new targets.CloudWatchLogGroup(logGroup)],
     });
